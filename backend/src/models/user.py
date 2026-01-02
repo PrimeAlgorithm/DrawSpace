273075858cedb5ec3cb3efb25c5e497b0e128aa1
PrimeAlgorithm <219721209+PrimeAlgorithm@users.noreply.github.com>
2026-01-01 18:51:23 -0800

    Update users.email

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy import String, DateTime, func
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, CITEXT
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
@@ -13,7 +13,7 @@ class User(Base):
         primary_key=True,
         default=uuid.uuid4, 
     )
-    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(CITEXT, nullable=False, unique=True)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
     first_name: Mapped[str] = mapped_column(String(100), nullable=False)
     last_name: Mapped[str] = mapped_column(String(100), nullable=False)
